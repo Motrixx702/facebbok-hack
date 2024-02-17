8f0044451fd6c23be53bd9cbf417ad08b098d6e0
Motrixx702 <150792247+Motrixx702@users.noreply.github.com>
2024-02-17 22:40:56 +0300

    Add files via upload

---
+++ facebbok-hacker.py
@@ -0,0 +1,19 @@
+print ("Welcome to the Facebook hacking tool")
+print ("Please answer some questions first to hack the account")
+v7x = input ("Is the account a personal or business account:")
+print (v7x)
+print ("So far so good")
+v7x2 = input ("Is the account existing or deleted:")
+print (v7x2)
+print ("This is amazing")
+v7x3 = input ("Enter the account name:")
+if v7x3 == "shahd Al-Hayek" :
+	print ("We will guess the account password and email")
+list100=0
+while list100 < 1000000 :
+	list100=list100+1
+	print (list100)
+print (" All information was successfully obtained")
+print ("This is the email 👉 sahedsy1234@gmail.com")
+print ("This is the password for the account👉sheddAlhaeeky483##@")
+
